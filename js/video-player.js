d83c0a19fb399e9691a991c5c24691c340750ab0
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-10-07 14:15:56 +0300

    recorder

--- js/video-player.js
+++ js/video-player.js
@@ -1,8 +1,14 @@
+const RecorderState = Object.freeze({
+    READY: 0,
+    RECORDING: 1,
+    PREVIEW: 2
+});
+
 const videoPlayer = document.getElementById('videoPlayer');
 const recordBtn = document.getElementById('recordBtn');
 recordBtn.focus();
 const recordingIndicator = document.getElementById('recordingIndicator');
-let isRecording = false;  // Track whether we are recording or not
+let recorderState = RecorderState.READY;
 
 function setFocusToRecordBtn() {
     resetPlayer();
@@ -11,7 +17,7 @@ function setFocusToRecordBtn() {
 
 // Function to start recording
 function startRecording() {
-    isRecording = true;
+    recorderState = RecorderState.RECORDING;
     recordBtn.textContent = 'Stop';  // Change the button text to 'Stop'
     recordBtn.classList.add('stop-btn');  // Change button color/style to indicate stop
 
@@ -22,16 +28,18 @@ function startRecording() {
     recordingIndicator.style.display = 'flex';
 
     // Start recording logic here
-    alert('Recording started! Implement your recording logic here.');
+    //alert('Recording started! Implement your recording logic here.');
 }
 
 // Function to stop recording and show video player controls
 function stopRecording() {
-    isRecording = false;
-    recordBtn.style.display = 'none';  // Hide the stop button after stopping
+    recorderState = RecorderState.PREVIEW;
+    recordBtn.textContent = 'Save';
+    recordBtn.classList.remove('stop-btn');
+    recordBtn.classList.add('save-btn');
 
     // Finalize recording logic here
-    alert('Recording stopped! Implement your finalizing logic here.');
+    //alert('Recording stopped! Implement your finalizing logic here.');
 
     // Hide the REC label with the pulsing dot
     recordingIndicator.style.display = 'none';
@@ -40,10 +48,11 @@ function stopRecording() {
     videoPlayer.setAttribute('controls', 'controls');
 }
 
-function resetPlayer(){
-    isRecording = false;
+function resetPlayer() {
+    recorderState = RecorderState.READY;
     recordBtn.textContent = 'Start recording';
     recordBtn.classList.remove('stop-btn');
+    recordBtn.classList.remove('save-btn');
     recordBtn.style.display = 'block';
     recordingIndicator.style.display = 'none';
     videoPlayer.removeAttribute('controls');
@@ -52,9 +61,13 @@ function resetPlayer(){
 
 // Event listener for record/stop button
 recordBtn.addEventListener('click', function () {
-    if (!isRecording) {
-        startRecording();  // Start recording if not already recording
-    } else {
-        stopRecording();  // Stop recording if it's currently recording
+    if (RecorderState.READY === recorderState) {
+        startRecording();
+    }
+    else if (RecorderState.RECORDING === recorderState) {
+        stopRecording();
+    }
+    else if (RecorderState.PREVIEW === recorderState) {
+        alert("save video");
     }
 });
