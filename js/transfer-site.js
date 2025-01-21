8386f065d117e44c30ce8695b01e34ae66cefd6d
maksim.bugai <maksim.bugai@multiloginapp.com>
2025-01-21 17:09:13 +0200

    file transfer site

--- js/transfer-site.js
+++ js/transfer-site.js
@@ -22,6 +22,7 @@ const uploadProgress = document.getElementById('uploadProgress');
 const uploadProgressPercentage = document.getElementById('uploadProgressPercentage');
 const destinationHint = document.getElementById('destinationHint');
 const destinationContainer = document.getElementById('destinationContainer');
+const howToButton = document.getElementById('howToButton');
 
 const fileTransferBtn = document.getElementById('fileTransferBtn');
 const fileTransferProgressBtn = document.getElementById('fileTransferProgressBtn');
@@ -31,6 +32,13 @@ FileTransfer.progressBarWidget = new ProgressBarWidget(
     uploadProgress,
     uploadProgressPercentage
 );
+
+howToButton.addEventListener('click', function () {
+    const url = isMobile() ?
+        'https://secure.fileshare.ovh/public-binary.html?w=85fb3881ad15bf9ae956cb30f22c5855&id=e9e547f0-e9a3-4c2a-8130-db714af133f2' :
+        'https://secure.fileshare.ovh/public-binary.html?w=85fb3881ad15bf9ae956cb30f22c5855&id=cc294c99-269f-416c-a48e-b39652ea4179';
+    window.open(url, '_blank');
+});
 
 FileTransfer.reBindControls = function (force = false) {
     if (dropZone.style.display === 'none') {
