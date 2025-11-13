b78fb53b1bc3b3115c5af3a894c8e3d32b3abb91
maksim.bugai <maksim.bugai@multiloginapp.com>
2025-11-13 14:01:20 +0200

    binary ping demo

--- js/similarity-captcha.js
+++ js/similarity-captcha.js
@@ -47,7 +47,7 @@ async function showCaptchaHintAndStart(captchaHint) {
     brandNameDiv.style.display = 'flex';
     if (captchaHint) {
         captchaHint.style.display = 'flex';
-        await delay(1000);
+        await delay(5000);
         captchaHint.remove();
     }
     await openWsConnection();
